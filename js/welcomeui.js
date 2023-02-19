0b64488fca539316bc5a0d1fbf7237a492b1d135
LiarleyCodie <liarley.personal@gmail.com>
2023-02-19 15:16:38 -0300

    issue: :beetle: popup behavior implemented but with problems

--- js/welcomeui.js
+++ js/welcomeui.js
@@ -1,3 +1,4 @@
+
 const dateTime = {
 	hourElement: document.querySelector("#hour"),
 	momentElement: document.querySelector("#momentText"),
@@ -36,14 +37,66 @@ const dateTime = {
 	}
 }
 
+const imageInformations = {
+	containerElement: document.querySelector("#informations"),
+	iconElement: document.querySelector("#pictureLink"),
+	popupElement: document.querySelector("#informationsBody"),
+	popupButtonElement: document.querySelector("#linkToUnsplash"),
+	state: "hidden",
+	timeout: null,
+	updateElement: function(state) {
+		if (state === "visible" || state === "hover") {
+			this.containerElement.classList.add("active")
+		} else if (state === "hidden") {
+			this.containerElement.classList.remove("active")
+		}
+	},
+	handle: function() {
+		addEventListener("mousemove", ({ target }) => {
+		if (target === this.containerElement ||
+			target === this.iconElement ||
+			target === this.popupElement ||
+			target === this.popupButtonElement) {
+			this.state = "hover"
+			clearTimeout(this.timeout)
+			this.timeout = null
+
+		}
+
+		if (this.state === "hover") {
+			if (target != this.containerElement &&
+				target != this.iconElement &&
+				target != this.popupElement &&
+				target != this.popupButtonElement) {
+				this.state = "visible"
+			}
+		}
+
+		this.updateElement(this.state)
+
+		if (this.state === "visible" && this.state != "hidden")
+			this.timeout = setTimeout(() => {
+				this.state = "hidden"
+				this.updateElement(this.state)
+				this.timeout = null
+			}, 2000)
+		})
+	}
+}
+
+
+// Initialize
 window.onload = () => {
 	dateTime.updateTime()
 	dateTime.updateTimeElement()
 	dateTime.updateTimeMomentElement()
 	dateTime.updateDate()
 	dateTime.updateDateElement()
+
+	imageInformations.handle()
 }
- 
+
+// date and time update
 setInterval(() => {
 	dateTime.updateTime()
 	dateTime.updateTimeElement()
@@ -51,7 +104,3 @@ setInterval(() => {
 	dateTime.updateDate()
 	dateTime.updateDateElement()
 }, 750)
-
-function Log(content) {
-	console.log(content)
-}
